9453bdb533d58076237bf294f485853568bfabea
Tiago Valinhas <tiago.valinhas@epitech.eu>
2026-04-25 11:58:22 +0200

    feat(api): aggregate population and area on regions and departments from communes

--- backend/common/src/main/java/com/homepedia/common/department/DepartmentRepository.java
+++ backend/common/src/main/java/com/homepedia/common/department/DepartmentRepository.java
@@ -3,9 +3,21 @@ package com.homepedia.common.department;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
 	Optional<Department> findByCode(String code);
 
 	List<Department> findByRegionCode(String regionCode);
+
+	@Modifying(flushAutomatically = true, clearAutomatically = true)
+	@Transactional
+	@Query(value = """
+			UPDATE departments SET
+			  population = (SELECT SUM(c.population) FROM cities c WHERE c.department_code = departments.code),
+			  area = (SELECT SUM(c.area) FROM cities c WHERE c.department_code = departments.code)
+			""", nativeQuery = true)
+	void recomputeAggregates();
 }

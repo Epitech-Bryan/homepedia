c62c65958778f63810e8627957c374ccb4a0fd36
BryanFRD <bryanferrando59@gmail.com>
2026-04-25 10:53:02 +0200

    fix(batch): remove @EnableBatchProcessing so spring boot creates metadata tables

--- backend/rest-api/src/main/java/com/homepedia/api/batch/config/BatchConfig.java
+++ backend/rest-api/src/main/java/com/homepedia/api/batch/config/BatchConfig.java
@@ -1,11 +1,9 @@
 package com.homepedia.api.batch.config;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableBatchProcessing
 @EnableScheduling
 public class BatchConfig {
 }

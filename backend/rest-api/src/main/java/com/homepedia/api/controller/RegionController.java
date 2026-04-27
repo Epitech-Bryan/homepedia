e44cb09a1d7e32dcddf8939949d03ea2eae29f97
William Smith <william.smith@epitech.eu>
2026-04-27 09:57:51 +0200

    fix: not checked is present in spring cache

--- backend/rest-api/src/main/java/com/homepedia/api/controller/RegionController.java
+++ backend/rest-api/src/main/java/com/homepedia/api/controller/RegionController.java
@@ -32,6 +32,8 @@ public class RegionController {
 	@Operation(summary = "Get a region by its code")
 	@GetMapping(BY_CODE)
 	public ResponseEntity<RegionSummary> findByCode(@PathVariable final String code) {
-		return regionService.findByCode(code).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+		return regionService.findByCode(code)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
 	}
 }

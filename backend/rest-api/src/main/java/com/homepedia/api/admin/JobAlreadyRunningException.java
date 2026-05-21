360c1478768225efd1064b0c6efcdbaa142fe676
BryanFRD <bryanferrando59@gmail.com>
2026-05-21 09:15:57 +0000

    chore(backend): clear compile warnings (text index, serialVersionUID, jackson typing)

--- backend/rest-api/src/main/java/com/homepedia/api/admin/JobAlreadyRunningException.java
+++ backend/rest-api/src/main/java/com/homepedia/api/admin/JobAlreadyRunningException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class JobAlreadyRunningException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public JobAlreadyRunningException(String jobName) {
 		super("Job '" + jobName + "' is already running");
 	}
